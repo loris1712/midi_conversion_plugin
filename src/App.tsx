eae0207605bb2ae00d81cafde90a979044fc1fa8
Archibold Bernard <aephisarh@gmail.com>
2024-07-05 08:54:56 +0000

    music page

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,33 @@ import UploadPage from './pages/Upload';
 import Navigation from './layout/Navigation';
 import HomePage from './pages/Home';
 import SettingsPage from './pages/Settings';
+import MusicPage from '@pages/Music';
 const  App = () => {
 
 
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <UploadPage />,
+      },
+      {
+        path: '/home',
+        element: <HomePage />,
+      },
+      {
+        path: '/music',
+        element: <MusicPage />,
+      },
+      {
+        path: '/setting',
+        element: <SettingsPage />,
+      },
+    ],
     {
-      path: '/',
-      element: <UploadPage />,
+      basename: '/',
     },
-    {
-      path: '/home',
-      element: <HomePage />,
-    },
-    {
-      path: '/setting',
-      element: <SettingsPage />,
-    },
-  ], {
-    basename: "/"
-  });
+  );
   
 
   return (
---
+++ src/pages/Music/index.tsx
@@ -0,0 +1,9 @@
+const MusicPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full">
+      <p className="text-white">This is the Music Page</p>
+    </div>
+  );
+};
+
+export default MusicPage
