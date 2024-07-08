7da012653e05e611417991ea1e90e47f679d6ece
Archibold Bernard <aephisarh@gmail.com>
2024-07-08 17:35:35 +0000

    allow drag

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,7 +1,8 @@
+import { Wrapper } from "./styles";
 const AppBar = () => (
-  <div className="h-[32px] flex flex-col bg-black items-center justify-center sticky w-screen top-0 left-0 right-0">
+  <Wrapper   className="h-[32px] flex flex-col bg-black items-center justify-center sticky w-screen top-0 left-0 right-0">
     <h4 className="text-appBar font-bold text-[13px]">Halbestunde</h4>
-  </div>
+  </Wrapper>
 );
 
 export default AppBar;
---
+++ src/components/AppBar/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+-webkit-app-region: drag;
+cursor: pointer;
+`
