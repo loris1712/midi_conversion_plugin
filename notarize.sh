e9ca8ee428b37df53636244ad6225689f017493d
Archibold Bernard <aephisarh@gmail.com>
2024-07-21 09:21:39 +0000

    version config

--- notarize.sh
+++ notarize.sh
@@ -1,1 +1,7 @@
 xcrun notarytool submit yourapp.zip --keychain-profile "YOUR_PROFILE_NAME" --wait
+
+ # /Users/archie/VisualStudioCode/Work/music-plugin/halbestunde-plugin/release/0.0.1/mac/Halbestunde.app
+
+# musedrm --protect /Users/archie/VisualStudioCode/Work/music-plugin/halbestunde-plugin/release/0.0.1/mac/Halbestunde.app --api_key 15e9599d096e0772e74b10ae7150050511e5bc5aad722bb3c8cacff589739fb8 --product_id plugin.halbestunde --overwrite
+
+# /Volumes/MuseDRM/musedrm --protect /Users/archie/VisualStudioCode/Work/music-plugin/halbestunde-plugin/release/0.0.2/mac/Halbestunde.app --api_key 15e9599d096e0772e74b10ae7150050511e5bc5aad722bb3c8cacff589739fb8 --product_id e556557a-d410-4733-a558-38bc0ab8b251  --overwrite
