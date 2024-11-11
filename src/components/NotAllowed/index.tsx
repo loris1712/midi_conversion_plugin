a0597b913ff98d5736e4d4f54d84b11feaed40b4
Archibold Bernard <aephisarh@gmail.com>
2024-11-11 15:37:50 +0000

    Unauthorized message

--- src/components/NotAllowed/index.tsx
+++ src/components/NotAllowed/index.tsx
@@ -8,8 +8,14 @@ const NotAllowed = () => {
   }, [])
     return (
       <div className="z-[100000] fixed top-0 right-0 left-0 bottom-0 h-screen w-screen bg-black/50 flex flex-col items-center justify-center backdrop-filter backdrop-blur-sm bg-opacity-10 ">
-        <h1 className="font-bold">Unauthorized</h1>
-        <p className="w-[] text-[12px] mt-4">Please authorize via the Muse Hub and try again!</p>
+        <h1 className="font-bold">
+          Please authorize via Muse Hub and try again!
+        </h1>
+        <ol className="flex flex-col items-center mt-4">
+          <li className="text-[12px]">1. Close this application.</li>
+          <li className="text-[12px]">2. Open Muse Hub.</li>
+          <li className="text-[12px]">3. Start this application again.</li>
+        </ol>
       </div>
     );
 }
